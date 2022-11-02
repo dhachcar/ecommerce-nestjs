9cfc2a72836597899e3251bbddac75d79a120e31
Danilo <dnl_pnd16@hotmail.com>
2022-11-02 17:41:39 -0300

    feat: iniciado auth.service / adicionado dependencias no auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { AuthService } from './services/auth.service';
 
-@Module({})
+@Module({
+  imports: [JwtModule, TypeOrmModule.forFeature([UserEntity])],
+  providers: [AuthService],
+})
 export class AuthModule {}
